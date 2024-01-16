e9c0e066b5f8e815606b111c8789a5c897a5cc2c
Zehra Şeren <fatmazehraseren@gmail.com>
2024-01-16 11:24:07 +0300

    Navbar created.

--- burger/src/App.js
+++ burger/src/App.js
@@ -4,10 +4,12 @@ import Home from "./Components/Home";
 import Menu from "./Components/Menu";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/menu" exact element={<Menu />} />
