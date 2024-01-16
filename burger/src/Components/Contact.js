8b04fc58f53975d59ca63e6e0d4d580d0cbcb287
Zehra Şeren <fatmazehraseren@gmail.com>
2024-01-16 20:25:24 +0300

    Contact created.

--- burger/src/Components/Contact.js
+++ burger/src/Components/Contact.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import BannerImage from "../assets/banner.png";
+import "../styles/contact.css";
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      ></div>
+      <div className="rightSide">
+        <h1>Bizimle iletişime geçin.</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Lütfen adınızı soyadınızı giriniz."
+          />
+          <label>E-mail</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Lütfen e-mail adresinizi giriniz."
+          />
+          <label>Mesajınız</label>
+          <textarea
+            rows="6"
+            name="message"
+            placeholder="Lütfen mesajınızı yazınız."
+          ></textarea>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
